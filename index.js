e75fc41a3e3ff7a267a24a74d6f55a035f0a05cf
liqiao <2276264677@qq.com>
2019-09-03 01:01:48 +0800

    封装路由功能，封装路由跳转工具函数

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 下面的代码是一段立即执行函数，用来动态计算高度，让页面适应不同的电脑的屏幕【使用了jquery方法，jquery已经在index.html中引入了】
 (function($) {
-  var resizeFn = function () {
+  var resizeFn = function (type) {
     // 获取浏览器窗口的高度
     var windowHeight = $(document).height();
     // console.log(windowHeight);
@@ -8,17 +8,19 @@
     var pageBox = $('#syspage');
     //设置pageBox的高度
     pageBox.css('height',windowHeight+'px')
+
+    if(type=='load') {
+      var hash = location.hash;
+      var path = hash.substring(1);
+      routerutil.route(path);
+    }
   }
-  window.onload = resizeFn;
-  window.onresize = resizeFn;
+
+  window.onload = resizeFn.bind(this,'load');
+  window.onresize = resizeFn.bind(this,'resize'); 
 })($);
 
+// 下面的routerutil已经注册到了全局对象 window中，所以可以直接使用了，跳转路由直接使用该方法
 function gotoPage(page) {
-  var iframe = $('#sub_page_container');
-  if(page == 'page_1') {
-    iframe.attr('src','./view/demo/html/demo.html')
-  }
-  if(page == 'page_2') {
-    iframe.attr('src','./view/demo2/html/demo.html')
-  }
+  routerutil.route(page);
 }
---
+++ routerConfig.js
@@ -0,0 +1,4 @@
+window.routers = [
+  {path:'page_1',templateurl:'./view/demo/html/demo.html'},
+  {path:'page_2',templateurl:'./view/demo2/html/demo.html'}
+];
