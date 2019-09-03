d898b54a43ce76e925cbd4b3c1519c0ec173a616
liqiao <2276264677@qq.com>
2019-09-03 10:15:22 +0800

    修复路由刷新导致回到起始页的bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 下面的代码是一段立即执行函数，用来动态计算高度，让页面适应不同的电脑的屏幕【使用了jquery方法，jquery已经在index.html中引入了】
 (function($) {
-  var resizeFn = function (type) {
+  var resizeFn = function () {
     // 获取浏览器窗口的高度
     var windowHeight = $(document).height();
     // console.log(windowHeight);
@@ -8,16 +8,9 @@
     var pageBox = $('#syspage');
     //设置pageBox的高度
     pageBox.css('height',windowHeight+'px')
-
-    if(type=='load') {
-      var hash = location.hash;
-      var path = hash.substring(1);
-      routerutil.route(path);
-    }
   }
-
-  window.onload = resizeFn.bind(this,'load');
-  window.onresize = resizeFn.bind(this,'resize'); 
+  window.addEventListener('load',resizeFn);
+  window.addEventListener('resize',resizeFn);
 })($);
 
 // 下面的routerutil已经注册到了全局对象 window中，所以可以直接使用了，跳转路由直接使用该方法
--- routerConfig.js
+++ routerConfig.js
@@ -1,3 +1,4 @@
+// 路由配置信息
 window.routers = [
   {path:'page_1',templateurl:'./view/demo/html/demo.html'},
   {path:'page_2',templateurl:'./view/demo2/html/demo.html'}
