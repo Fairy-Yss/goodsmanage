28c73c26328faae61f1f2a8940c28982d1b0e94d
Fairy-Yss <1105317460@qq.com>
2019-09-03 05:25:47 -0700

    写了一丢丢界面

--- routerConfig.js
+++ routerConfig.js
@@ -1,5 +1,7 @@
 // 路由配置信息
 window.routers = [
   {path:'page_1',templateurl:'./view/demo/html/demo.html'},
-  {path:'page_2',templateurl:'./view/demo2/html/demo.html'}
+  {path:'page_2',templateurl:'./view/demo2/html/demo.html'},
+  {path:'page_3',templateurl:'./view/demo2/html/demo.html'},
+  {path:'page_4',templateurl:'./view/demo2/html/demo.html'}
 ];
